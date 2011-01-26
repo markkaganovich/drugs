e2acdcf823f097ecce1717e12035a7988fb5f861
Mark Kaganovich <mark@labmeeting.com>
2011-01-25 17:57:42 -0800

    work on unique SNPs 2010 genotype file, copy of indelbarcode.py

---
+++ uniqueSNPs.py
@@ -0,0 +1,84 @@
+# ----------------------------------------------------------------------------------
+#
+# Mark Kaganovich,  May 3, 2010
+#
+# find stretches of the genome that will identify 1000genome lines uniquely based on
+# the recently released SNP calls
+#
+# -----------------------------------------------------------------------------------
+
+from helperfuns_findbarcode import *
+import simplejson
+import logging
+
+#def findbarcode(population, file, dnastretch = 300):
+#FILENAME = './'+population+'.genotypes.vcf'
+#FILENAME = './test'
+#FILENAME = './partial_input'
+#FILENAME = './input' + population +'/SNPs_chrom1'
+#   FILENAME = '../Desktop/release_20100608.CEU-merge.genotypes.phased_haplotypes.no_trio.vcf'
+#    FILENAME = './temp'
+#FILENAME = '../Desktop/CEU.low_coverage.indel.vcf'
+#    FILENAME = './chr1314SNPs.txt'
+    population = 'CEU'
+    DATABYTES = 10000000
+#DNASTRETCH = dnastretch
+    POS = 1
+    REF = 3
+    ALT = 4
+    file = '../1000GenomesData/CEU.low_coverage.2010_09.genotypes.vcf'   
+    file = open(file)
+    outputfile = open('./'+population+'output', 'w')
+
+# read through header
+    lines = file.readlines(DATABYTES)
+    for z in range(0, len(lines)):
+        print z
+        if lines[z].startswith('#CHROM'):
+            print lines[z]
+            NUM_GENOMES = len(list(lines[z].split('\t')[9:-1]))
+            print NUM_GENOMES         
+            lines = lines[z+1:]
+            break
+        else:
+            pass
+
+    barcode_snps=[]
+    regions=[]
+    genomic_positions=[]
+    j = 0
+# read blocks of data at a time: DATABYTES at a time
+    while(1):
+        if not lines:
+            break
+        print j
+        print 'lines:  '+str(lines[j].split('\t')[0])
+        genotype = vcf_to_geno_struct([lines[j]])
+        if genotype:
+            [num_genos, cell_line_ID] =num_genotypes(genotype)
+            print num_genos
+            hg18pos_start = int(lines[j].split('\t')[POS])
+            hg18seq = str(lines[j].split('\t')[REF])
+            varseq = str(lines[j].split('\t')[ALT])
+            chr = int(lines[j].split('\t')[0])
+            hg18pos_end = hg18pos_start + len(hg18seq)
+#print hg18seq
+#           print len(hg18seq)
+            if num_genos == 1:
+                barcode_snps.append([j, cell_line_ID, chr, hg18pos_start, hg18pos_end, hg18seq, varseq])
+#simplejson.dump([genomic_positions, regions], outputfile) 
+
+        if len(lines) <= j+1:
+            lines.extend(file.readlines(DATABYTES))
+            if len(lines) <= j+1:                                # if reading more lines does not extend LINES, done
+                break
+        j = j+1
+#[genomic_positions, regions] = get_rid_of_redundancy(regions, genomic_positions)
+#   simplejson.dump([genomic_positions, regions], outputfile)                
+    file.close()
+    outputfile.close()
+
+    return barcode_snps
+
+
+
