f1b1e7577bf933579e5111c92c36817f480e3fa9
markkaganovich <mark.kaganovich@gmail.com>
2011-02-10 11:09:57 -0800

    vdftomongo

--- vcftomongo.py
+++ vcftomongo.py
@@ -9,9 +9,9 @@ file = open('../1000GenomesData/CEU.low_coverage.2010_09.genotypes.vcf')
 lines = []
 
 while(not lines):
-    lines = file.readlines(10000000)
-    print lines[0]
+    lines = file.readlines(1000000000)
     if not lines: break;
+    print lines[0]
     for i in range(0, len(lines)):
         line = lines[i].split('\t')
         if str(line[0]) == '#CHROM':
@@ -31,6 +31,38 @@ while(not lines):
     print i 
             
 file.close() 
+
+# add info from other files
+
+file = open('../1000GenomesData/CHBJPT.low_coverage.2010_09.genotypes.vcf')
+lines = file.readlines()
+
+CHROM = 0
+POS = 1
+
+newSNPs = db.newSNPs
+
+for line in lines:
+	line = line.split('\t')
+	if str(line[0]) == '#CHROM':
+        cell_lines = line[9:]
+    if '#' not in str(line[0]):
+        tmp = SNPs.find({'#CHROM': line[CHROM]})
+        if tmp:
+            record = {}
+            for par in parameters:
+                record[str(par)] = line[parameters.index(par)]
+            individuals_record = {}
+            for individual in cell_lines:
+                individuals_record[str(individual)] = line[cell_lines.index(individual)+9]
+            record['individuals'] = individuals_record    
+            newSNPs.insert(record)
+        else:
+            indtemp = tmp['individuals']
+            for individual in cell_lines:
+                indtemp[str(individual)] = line[cell_lines.index(individual)+9]
+            tmp['individuals'] = indtemp
+            
 
 
 
