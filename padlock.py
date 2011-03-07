d6c8e91d8efe832a947d82721b0e3291ae7516c5
Mark Kaganovich <mark@labmeeting.com>
2011-03-07 12:20:26 -0800

    updated padlock to run again

--- padlock.py
+++ padlock.py
@@ -8,17 +8,29 @@ OPT_TEMP_E = 53
 BACKBONE = 32
 LENGTH = 24
 
+'''
+11 bp gap between ligation and extension strands
+look for primers that are within 1 degree of optimal Tms
+'''
+
 class Primer:
     def __init__(self, chr, pos):
     	self.chr = chr
         self.pos = pos    
 
+    def ligpos():
+        return [self.pos-30, self.pos-5]
+    
+    def extpos():
+        return [self.pos+5,self.pos+28]
 
     def checktemp(self):
         if self.pos-28 <= 1:
             return False
-        tmlig = MeltingTemp.Tm_staluc(str(hg18['chr'+str(self.chr)][self.pos-29:self.pos-4]))
-        tmext = MeltingTemp.Tm_staluc(str(hg18['chr'+str(self.chr)][self.pos+4:self.pos+27]))
+        lpos = ligpos()
+        epos - extpos()
+        tmlig = MeltingTemp.Tm_staluc(str(hg18['chr'+str(self.chr)][lpos[0]:lpos[1]]))
+        tmext = MeltingTemp.Tm_staluc(str(hg18['chr'+str(self.chr)][epos[0]:epos[1]]))
         print tmlig, tmext
 
         if abs(tmlig - OPT_TEMP_L) <=1 and abs(tmext - OPT_TEMP_E) <= 1:
@@ -50,18 +62,18 @@ if __name__ == "__main__":
     b = simplejson.load(file)
     file.close()
 
-    
-    trueprobes2 = []
-    i = 0
-    for snp in b[1]:
-	i=i+1
-	print i
-	if Primer(snp[1],snp[2]).checktemp():
-            trueprobes2.append(snp)
-	
-    file = open('./trueprobes2','w')
-    simplejson.dump(trueprobes2, file)
-    file.close()
+    for j in range(0,len(b)):
+        trueprobes = []
+        i = 0
+        for snp in b[j]:
+            i = i+1
+            print i
+            if Primer(snp[1], snp[2]).checktemp():
+                trueprobes.append(snp)
+
+        file = open('./checkedpadlocks'+str(j),'w')
+        simplejson.dump(trueprobes, file)
+        file.close()
 
     	
 	
