726a8d98178d76999f202a578402f4c007f0f3cb
Mark Kaganovich <mark.kaganovich@gmail.com>
2011-07-26 16:14:26 -0400

    working checkilluminareads

--- padlock.py
+++ padlock.py
@@ -161,6 +161,58 @@ def printprobesandorderthem(probesfile, probesoutputfile):
     file.close()
     
 
+def getsnpregionseq(probe):
+
+    return str(hg18['chr'+str(probe[1])][int(probe[2])-30 : int(probe[2])+28])
+
+def checkilluminasnps(probesfile, probetype = 1, readsfile = '110617_MONK_0184_B81N3EABXX_L4_eland_extended_pf.txt'):
+    import simplejson
+
+    file = open(probesfile)
+    probes = simplejson.load(file)
+    file.close()
+    vars = [0] * len(probes)   
+    wts = [0] * len(probes)
+    others = [0] * len(probes)
+    varinds = [[]] * len(probes)
+
+    reads = open('./'+readsfile)
+    lines = reads.readlines(1000000)
+    
+    while(lines != []):
+        for i in range(0,len(probes)):
+            probeseq = getsnpregionseq(probes[i])
+            probeseq = probeseq.upper()
+            for l in lines:
+                read = l.split('\t')[1]
+                mismatches = len(probeseq) - len(filter(lambda x: probeseq[x] == read[x], range(0,len(probeseq))))
+                if probeseq[29] == read[29] and mismatches <= 2:
+                    wts[i] = wts[i]+1
+                if probeseq[29] != read[29] and mismatches <= 3:
+                    vars[i] = vars[i] +1
+                    varinds[i].append(l)
+                if mismatches <= 2:
+                 others[i] = others[i] + 1
+        lines = reads.readlines(1000000)    
+    
+    return [wts, vars, varinds, others]
+
+def findnumalleles(probe, genotypefile):
+    probechr = int(probe[1])
+    probepos = int(probe[2])
+
+    print probechr
+    print probepos
+
+    file = open(genotypefile)
+    lines = file.readlines()
+    file.close()
+
+    for l in lines:
+        line = l.split('\t')
+        if l.startswith('#') != True and int(line[0]) == probechr and int(line[1]) == probepos:
+            return int(line[7].split(';')[1].split('=')[1])
+
 '''
 probeset : the probes that pass the blast test
 alreadyblasted : the probes that have already been blasted
